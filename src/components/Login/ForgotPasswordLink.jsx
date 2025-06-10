8ae3268adb184edeec91a8879b7981af4f4c50f5
codewithamasu2 <ussmh6697@gmail.com>
2025-06-11 01:28:26 +0700

    Delete src directory

--- src/components/Login/ForgotPasswordLink.jsx
+++
@@ -1,9 +0,0 @@
-export default function ForgotPasswordLink() {
-  return (
-    <div className="text-center">
-      <a href="#" className="text-[#248CBC] hover:underline text-sm">
-        Forgot Password?
-      </a>
-    </div>
-  );
-}
